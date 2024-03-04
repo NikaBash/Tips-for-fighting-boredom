9c2b8eb6fdcf1a17195e0e0ff33131ff00e20de9
NikaBash <veronika-0509@yandex.com>
2024-03-04 23:07:26 +0300

    project to hone skills in API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import video from './book.mp4';
+
 
 function App() {
+
+  const [myAdvice, setMyAdvice] = useState('');
+
+  useEffect(() => {
+    getAdvice();
+    }, [])
+
+  const getAdvice = async () => {
+    const response = await fetch (`http://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setMyAdvice(data.activity);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <div className="container">
+  <video autoPlay muted loop>
+    <source src={video} type="video/mp4"/>
+  </video>
+  <h1>Tips for fighting boredom</h1>
+  </div>
+
+  <div className='container'>
+    <p>{myAdvice}</p>
+  </div>
+  
+  <div className='container'>
+  <button onClick={getAdvice}>New Tip</button>
+  </div>
+  </div>
   );
 }
 
